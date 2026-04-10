b0e9dce5f78ad506f5c2ecc61cf16e8cbca8f6fb
hackevin <hackevin@users.noreply.github.com>
2026-04-10 15:12:08 +0800

    Add files via upload

---
+++ cm10086_noad.js
@@ -0,0 +1,17 @@
+(function () {
+  // 中国移动首页净化（思路级）
+  body = JSON.parse($response.body);
+
+  // 删除常见广告字段
+  delete body.ads;
+  delete body.adList;
+  delete body.bannerList;
+  delete body.activityList;
+
+  // 过滤推荐位
+  if (body.data && body.data.cards) {
+    body.data.cards = body.data.cards.filter(i => !i.type.includes("ad"));
+  }
+
+  $done({body: JSON.stringify(body)});
+})();
