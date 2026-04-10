2957f06f161cadd523500acc44670fbf26f12336
hackevin <hackevin@users.noreply.github.com>
2026-04-10 15:19:45 +0800

    Update cm10086_noad.js

--- cm10086_noad.js
+++ cm10086_noad.js
@@ -4,18 +4,35 @@
 
 (function () {
   // 中国移动首页净化（思路级）
-  body = JSON.parse($response.body);
-
-  // 删除常见广告字段
-  delete body.ads;
-  delete body.adList;
-  delete body.bannerList;
-  delete body.activityList;
-
-  // 过滤推荐位
-  if (body.data && body.data.cards) {
-    body.data.cards = body.data.cards.filter(i => !i.type.includes("ad"));
+  let body = $response.body;
+  
+  try {
+    let obj = JSON.parse(body);
+  
+    // 常见广告字段清理
+    if (obj.data) {
+      delete obj.data.ad;
+      delete obj.data.ads;
+      delete obj.data.banner;
+      delete obj.data.adList;
+      delete obj.data.activityList;
+    }
+  
+    // 递归过滤（弱版本）
+    function clean(o) {
+      if (Array.isArray(o)) {
+        return o.filter(i => !i?.type?.includes("ad"));
+      }
+      return o;
+    }
+  
+    if (obj.data && obj.data.cards) {
+      obj.data.cards = clean(obj.data.cards);
+    }
+  
+    $done({ body: JSON.stringify(obj) });
+  
+  } catch (e) {
+    $done({});
   }
-
-  $done({body: JSON.stringify(body)});
 })();
