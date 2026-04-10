451c3940df684d722c8d19e8a17751cfa05eb756
hackevin <hackevin@users.noreply.github.com>
2026-04-10 13:24:58 +0800

    Update meituan_clean.js

--- meituan_clean.js
+++ meituan_clean.js
@@ -1,11 +1,21 @@
 (function () {
-  let obj = JSON.parse($response.body);
-
-  if (obj.data && obj.data.data) {
-    obj.data.data = obj.data.data.filter(i => {
-      return !i.hasOwnProperty("ad");
-    });
+  try {
+    let obj = JSON.parse($response.body);
+  
+    if (obj.data?.data) {
+      obj.data.data = obj.data.data.filter(i => {
+        return !i.ad && !i.is_ad;
+      });
+    }
+  
+    // 去顶部banner
+    if (obj.data?.banner) {
+      delete obj.data.banner;
+    }
+  
+    $done({ body: JSON.stringify(obj) });
+  
+  } catch (e) {
+    $done({});
   }
-
-  $done({ body: JSON.stringify(obj) });
 })();
