accd69e73a52276f7d5ac82e0574a83b747ea2b2
hackevin <hackevin@users.noreply.github.com>
2026-04-10 13:24:10 +0800

    Update bilibili_clean.js

--- bilibili_clean.js
+++ bilibili_clean.js
@@ -1,11 +1,21 @@
 (function () {
-  let obj = JSON.parse($response.body);
-
-  if (obj.data && obj.data.items) {
-    obj.data.items = obj.data.items.filter(item => {
-      return !item.hasOwnProperty("ad_info");
-    });
+  try {
+    let obj = JSON.parse($response.body);
+  
+    if (obj.data?.items) {
+      obj.data.items = obj.data.items.filter(i => {
+        return !i.ad_info && !i.card_goto?.includes("ad");
+      });
+    }
+  
+    // 去 banner
+    if (obj.data?.banner) {
+      delete obj.data.banner;
+    }
+  
+    $done({ body: JSON.stringify(obj) });
+  
+  } catch (e) {
+    $done({});
   }
-
-  $done({ body: JSON.stringify(obj) });
 })();
