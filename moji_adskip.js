1e56d4d3f100ef8819e7182c941b136cd501cd70
hackevin <hackevin@users.noreply.github.com>
2026-04-10 10:31:10 +0800

    Update moji_adskip.js

--- moji_adskip.js
+++ moji_adskip.js
@@ -3,22 +3,30 @@
  * 修改自 ddgksf2013
  */
 
-let obj = JSON.parse($response.body);
-
-if ($request.url.indexOf("v6/ad/get_ad") !== -1) {
-    if (obj.data && obj.data.ad_list) {
-        obj.data.ad_list = []; // 清空广告列表
+(function () {
+    let obj;
+    try {
+        obj = JSON.parse($response.body);
+    } catch (e) {
+        $done({});
+        return;
     }
-    if (obj.data && obj.data.common_ad) {
-        obj.data.common_ad = {}; 
-    }
-}
 
-if ($request.url.indexOf("v6/config/get_config") !== -1) {
-    // 移除配置中的开屏广告链接
-    if (obj.data && obj.data.splash_config) {
-        delete obj.data.splash_config;
+    if ($request.url.indexOf("v6/ad/get_ad") !== -1) {
+        if (obj.data && obj.data.ad_list) {
+            obj.data.ad_list = []; // 清空广告列表
+        }
+        if (obj.data && obj.data.common_ad) {
+            obj.data.common_ad = {};
+        }
     }
-}
 
-$done({ body: JSON.stringify(obj) });
+    if ($request.url.indexOf("v6/config/get_config") !== -1) {
+        // 移除配置中的开屏广告链接
+        if (obj.data && obj.data.splash_config) {
+            delete obj.data.splash_config;
+        }
+    }
+
+    $done({ body: JSON.stringify(obj) });
+})();
