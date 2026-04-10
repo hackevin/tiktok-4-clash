f159206a094ac042d8f939c0135472080846f8c3
hackevin <hackevin@users.noreply.github.com>
2026-04-10 13:24:35 +0800

    Enhance ad filtering in jd_clean.js
    Refactor ad filtering logic to use optional chaining and improve readability.

--- jd_clean.js
+++ jd_clean.js
@@ -1,11 +1,23 @@
 (function () {
-  let obj = JSON.parse($response.body);
-
-  if (obj.data && obj.data.floorList) {
-    obj.data.floorList = obj.data.floorList.filter(i => {
-      return !i.hasOwnProperty("ad");
-    });
+  try {
+    let obj = JSON.parse($response.body);
+  
+    if (obj.data?.floorList) {
+      obj.data.floorList = obj.data.floorList.filter(i => {
+        return !i.ad && !i.bizCode?.includes("ads");
+      });
+    }
+  
+    // 搜索广告
+    if (obj.data?.searchm?.Paragraph) {
+      obj.data.searchm.Paragraph = obj.data.searchm.Paragraph.filter(i => {
+        return !i.ad;
+      });
+    }
+  
+    $done({ body: JSON.stringify(obj) });
+  
+  } catch (e) {
+    $done({});
   }
-
-  $done({ body: JSON.stringify(obj) });
 })();
